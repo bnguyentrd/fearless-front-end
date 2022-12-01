594b6d1db4978add957cbcc4ed6ffb380636d528
Bobby <bnguyentrd@gmail.com>
2022-12-01 11:20:54 -0800

    Routing Everything

---
+++ ghi/app/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Nav from "./Nav";
+import AttendeesList from './AttendeesList';
+import LocationForm from './LocationForm';
+import ConferenceForm from './ConferenceForm';
+import AttendeeForm from './AttendeeForm';
+
+function App(props) {
+  if (props.attendees === undefined) {
+    return null;
+  }
+  return (
+    <>
+      <Nav />
+      <div className="container">
+        <AttendeeForm />
+        {/* <ConferenceForm /> */}
+        {/* <LocationForm /> */}
+        {/* <AttendeesList attendees={props.attendees} /> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
--- monolith/conference_go/settings.py
+++ monolith/conference_go/settings.py
@@ -70,7 +70,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://localhost:3001",
 ]
 
 ROOT_URLCONF = "conference_go.urls"
